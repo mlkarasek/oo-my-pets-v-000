7c05abbf54b270f26fcd89cf123ddeb52a1b9e2c
Monica  Karasek <mlkarasek@gmail.com>
2017-10-27 22:49:40 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,36 @@
 class Owner
-  # code goes here
-end
+
+  attr_accessor :owner, :pets
+  attr_reader :species
+
+  @@all = []
+
+  def initialize(name, pets, species)
+    @name = name
+    @pets = {fishes: [], cats: [], dogs: []}
+    @species = "human"
+    @@all << self
+  end
+
+  def buy_cat(name)
+    new_kitty = Cat.new(name)
+    @pets[:cats] << new_kitty
+  end
+
+  def buy_dog(name)
+    new_puppy = Dog.new(name)
+    @pets[:dogs] << new_puppy
+  end
+
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    @pets[:fishes] << new_fish
+  end
+
+  def walk_dogs
+    @pets[:dogs].each do |dogs|
+      dogs.mood = "happy"
+    end
+  end
+
+end 
