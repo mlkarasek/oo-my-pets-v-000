dccf8bc535adde9d56712c47b5950933222a8099
Monica  Karasek <mlkarasek@gmail.com>
2017-10-27 23:04:03 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -17,12 +17,16 @@ class Owner
   end
 
   def self.reset_all
-    @all.reset_all
+    @@all.clear
   end
 
   def self.count
     @@all.size
   end
+
+  def say_species
+    "I am a #{@species}."
+  end 
 
   def buy_cat(name)
     new_kitty = Cat.new(name)
